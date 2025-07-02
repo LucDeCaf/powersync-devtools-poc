b68dea682a9bf5a7612fa50798bc0a6a8081c793
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2025-07-02 17:45:50 +0200

    rename imports

--- src/devtools/panel/context/ConnectorContext.ts
+++ src/devtools/panel/context/ConnectorContext.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { Connector } from '../connector/Connector';
+import { Connector } from '../adapters/Connector';
 
 export const ConnectorContext = createContext<Connector>(new Connector());
 export const useConnector = () => useContext(ConnectorContext);
